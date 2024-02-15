9df8f7881e8c8f8259de0673f79222d7206b5084
Njabulo <nnshiwu@gmail.com>
2024-02-15 22:13:37 +0200

    Added a return statement that returns encrypted_text.

--- main.py
+++ main.py
@@ -19,3 +19,4 @@ def vigenere(message,key):
          index = alphabet.find(char)
          new_index = (index + offset) % len(alphabet)
          encrypted_text += alphabet[new_index]
+   return encrypted_text
