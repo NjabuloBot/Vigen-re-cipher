34494c10348353b774f75a9459996bfeb168253d
Njabulo <nnshiwu@gmail.com>
2024-02-15 22:42:31 +0200

    Turned direction to a default argument,by assigning 1 to direction.

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 text = input()
 custom_key = 'python'
 
-def vigenere(message,key,direction):
+def vigenere(message,key,direction = 1):
    key_index = 0
    alphabet = 'abcdefghijklmnopqrstuvwxyz'
    final_message = ''
